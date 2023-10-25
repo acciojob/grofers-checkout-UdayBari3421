0f1eacb95f2868f6ac1748e6efeb9c53c2e49d3d
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-10-25 09:36:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,6 @@ const getSumBtn = document.createElement("button");
 const table = document.querySelector("table");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-const  item = document.querySelectorAll(".price");
-var arr = [];
-item.forEach((e)=>{
-	let value = e.innerText; 
-	arr.push(parseInt(value));
-});
 
 let tr = document.createElement("tr");
 tr.setAttribute("id","ans");
