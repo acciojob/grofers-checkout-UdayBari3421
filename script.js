66cfdd098ef16631a5a2c3cb4eb9d6809919560c
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-10-25 09:27:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,15 +10,19 @@ item.forEach((e)=>{
 });
 
 let tr = document.createElement("tr");
+let tr1 = document.createElement("tr");
 tr.setAttribute("id","ans");
+tr1.setAttribute("id","ans");
 const getSum = () => {
 	let sum = 0;
 
 	for (let i = 0; i < arr.length; i++) {
 		sum = sum + arr[i];
 		tr.innerText = sum+10;
+		tr1.innerText = sum - 67;
 	}	
 };
 table.appendChild(tr);
+table.appendChild(tr1);
 getSumBtn.addEventListener("click", getSum);
 
